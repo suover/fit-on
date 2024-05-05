888d7b0587e934884166831dc3a46d82f65693ea
gtg9696 <101083773+HOSIHO@users.noreply.github.com>
2024-05-05 23:46:14 +0900

    Update App.tsx

--- src/main/reactfront/src/App.tsx
+++ src/main/reactfront/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import GlobalStyle from './styles/common/GlobalStyle';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
