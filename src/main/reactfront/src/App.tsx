ba2ac282c106880a130d3b1bf5ddfbb0dc7fc800
sol-ee <159120751+sol-ee@users.noreply.github.com>
2024-05-18 18:26:57 +0900

    Update package.json

--- src/main/reactfront/src/App.tsx
+++ src/main/reactfront/src/App.tsx
@@ -12,6 +12,11 @@ import GlobalStyle from './styles/common/GlobalStyle';
 import Header from './components/layout/header/Header';
 import Footer from './components/layout/footer/Footer';
 import MuiFontTheme from './styles/common/MuiFontTheme';
+import MainProduct from './pages/mall/mainProduct';
+import Fitness from './pages/mall/fittness';
+import Supplement from './pages/mall/supplemnet';
+import Food from './pages/mall/food';
+import ProductDetail from './pages/productDetail/productDetail';
 
 const App = () => {
   return (
@@ -21,16 +26,16 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Community />} />
-
           <Route path="/post/:postNo" element={<ViewPostDetail />} />
-
           <Route path="/new-post" element={<NewPost />} />
-
           <Route path="/routine" element={<RoutineMain />} />
-
           <Route path="/routine/:routineNo" element={<ViewRoutineDetail />} />
-
-          <Route path="/new-routine" element={<NewRoutine />} />
+          <Route path="/new-routine" element={<NewRoutine />} />4{' '}
+          <Route path="/mainProduct" element={<MainProduct />} />
+          <Route path="/fitness" element={<Fitness />} />
+          <Route path="/supplement" element={<Supplement />} />
+          <Route path="/food" element={<Food />} />
+          <Route path="/productDetail" element={<ProductDetail />} />
         </Routes>
       </Router>
       <Footer />
