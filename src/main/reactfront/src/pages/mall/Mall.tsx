5dcbcc8c6e99527c8b7bbce8453eda4c698b4a43
HOSIHO <compleat777@gmail.com>
2024-07-15 03:57:21 +0900

    :recycle: Mall.tsx
    로컬스토리지에 저장된 유저ID 기반,사용자 장바구니에 담긴 상품 수량 반영

--- src/main/reactfront/src/pages/mall/Mall.tsx
+++ src/main/reactfront/src/pages/mall/Mall.tsx
@@ -31,6 +31,7 @@ const Mall: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [cartItemCount, setCartItemCount] = useState(0);
   const pageSize = 12;
 
   // 장바구니만 route로 적용시키고, 나머지는 카테고리 필터링으로 변경
@@ -51,13 +52,38 @@ const Mall: React.FC = () => {
       route: '/shopping-basket',
       menuName: '장바구니',
       icon: ShoppingCartIcon,
-      badge: 3,
+      badge: cartItemCount,
     },
   ];
+  // 로컬 스토리지에서 userId를 가져오기
+  const getUserIdFromLocalStorage = (): number | null => {
+    const storedUserId = localStorage.getItem('userId');
+    return storedUserId ? parseInt(storedUserId, 10) : null;
+  };
 
-  //상품 정보 세팅
+  // 장바구니 상품 수량 가져오기
+  const fetchCartItemCount = async (userId: number) => {
+    try {
+      const response = await axios.get(`/api/carts/${userId}/cartItems`);
+      if (response.status === 200) {
+        setCartItemCount(response.data.length);
+      } else {
+        console.error('Failed to fetch cart items count');
+      }
+    } catch (error) {
+      console.error('Error fetching cart items count', error);
+    }
+  };
+
+  // 상품 정보 세팅
   useEffect(() => {
     fetchProducts();
+    const userId = getUserIdFromLocalStorage();
+    if (userId) {
+      fetchCartItemCount(userId);
+    } else {
+      console.error('No user ID found in local storage');
+    }
   }, []);
 
   //상품 정보 가져오기
