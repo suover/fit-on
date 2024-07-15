216fec2c7a3eaa88509c62d10bf4e1b5db79f4ea
HOSIHO <compleat777@gmail.com>
2024-07-15 20:57:16 +0900

    :recycle: ImageDetail.tsx
    상품 추가 이미지 반영

--- src/main/java/com/spring/myapp/product/controller/ProductController.java
+++ src/main/java/com/spring/myapp/product/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.spring.myapp.product.controller;
 
+import com.spring.myapp.cart.controller.CartController;
 import com.spring.myapp.product.model.Product;
 import com.spring.myapp.product.service.ProductService;
 import com.spring.myapp.product.service.S3Service;
 
 import org.apache.ibatis.annotations.Update;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,7 @@ import java.util.Map;
 @RequestMapping("/api/products")
 @Validated
 public class ProductController {
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
 	@Autowired
 	private S3Service s3Service;
