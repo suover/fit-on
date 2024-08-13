df6b0941d680232bb2e50bccf7ef0ce0c1194c00
Min Su Kim <gts2044@gmail.com>
2024-08-14 03:54:00 +0900

    🛠️ Chore: 프로젝트 배포를 위한 환경 설정
    - Dockerfile을 작성하여 애플리케이션의 배포 환경을 설정
    - HealthCheckController를 추가하여 서버 상태와 환경 정보를 확인할 수 있도록 구현
    - 기존 application.yml을 환경별로 구성하여 local, blue, green 프로파일 설정
    - 중요한 정보는 application-secret.yml로 분리하여 GitHub Secrets로 관리
    - 배포 시 환경별로 정확한 설정이 적용되도록 함

--- src/main/reactfront/.eslintrc.ts
+++ src/main/reactfront/.eslintrc.ts
@@ -30,5 +30,7 @@ module.exports = {
   rules: {
     "prettier/prettier": ["error"],
     "no-param-reassign": ["error", { props: false }],
+    "no-unused-vars": "off",
+    "react-hooks/exhaustive-deps": "off",
   },
 };
