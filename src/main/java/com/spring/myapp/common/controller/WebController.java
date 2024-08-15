a405df682a58fb631d374187d774c6bcdf9273d1
Min Su Kim <gts2044@gmail.com>
2024-08-16 04:28:03 +0900

    🛠️ Chore: 프로젝트 배포를 위한 환경 설정
    - Dockerfile을 작성하여 애플리케이션의 배포 환경을 설정
    - HealthCheckController를 추가하여 서버 상태와 환경 정보를 확인할 수 있도록 구현
    - 기존 application.yml을 환경별로 구성하여 local, blue, green 프로파일 설정
    - 중요한 정보는 application-secret.yml로 분리하여 GitHub Secrets로 관리
    - 배포 시 환경별로 정확한 설정이 적용되도록 함

--- src/main/java/com/spring/myapp/common/controller/WebController.java
+++ src/main/java/com/spring/myapp/common/controller/WebController.java
@@ -3,10 +3,17 @@ package com.spring.myapp.common.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController implements ErrorController {
-	@GetMapping({"/", "/error"})
+
+	@RequestMapping("/error")
+	public String handleError() {
+		return "forward:/";
+	}
+
+	@GetMapping({"/", "/index.html"})
 	public String index() {
 		return "index.html";
 	}
