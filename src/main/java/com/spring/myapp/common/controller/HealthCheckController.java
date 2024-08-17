47e9bbbe1b5a8774350e3b256dae18e72730ca79
Min Su Kim <gts2044@gmail.com>
2024-08-17 21:08:48 +0900

    🛠️ Chore: 배포 및 CI/CD 환경 설정 완료
    - CI/CD 파이프라인 구성
        - `main` 브랜치에 대한 푸시 발생 시 GitHub Actions를 통해 CI/CD가 자동으로 실행되도록 설정.
        - 환경 변수를 설정하고 React 프론트엔드와 Spring Boot 백엔드의 빌드를 진행.
        - Docker 이미지를 빌드하고 DockerHub에 최신 이미지를 푸시하는 단계를 추가하여 최신 상태를 유지.
        - 배포 단계에서 현재 서버의 상태를 확인하고, 새로운 버전을 무중단 배포하는 방식으로 Nginx Upstream을 전환하도록 구성.
    - Docker & Nginx
        - `Dockerfile`을 작성하여 애플리케이션의 배포 환경을 Docker 기반으로 설정.
        - AWS EC2에서 Docker와 Nginx를 사용하여 Blue/Green 배포 전략을 활용한 무중단 배포 구현.
        - 서버의 현재 상태를 확인하고, 업스트림을 적절히 설정하여 배포 진행.
        - Nginx를 통해 업스트림을 동적으로 변경하고, 배포 후 사용하지 않는 서버를 정리.
        - `default.conf`에 403, 404 오류 발생 시 프론트엔드 SPA 라우팅 문제를 해결하기 위한 설정 추가.
    - 애플리케이션 설정
        - Spring Boot의 `application.yml` 파일을 환경별로 나누어 `local`, `blue`, `green` 프로파일을 설정.
        - `HealthCheckController`를 추가하여 서버 상태와 환경 정보를 확인할 수 있도록 구현.
        - `WebController`를 통해 SPA의 클라이언트 사이드 라우팅 문제를 해결.
        - CORS 설정을 업데이트하여 로컬 개발 환경과 배포 환경 모두에서 API 호출이 가능하도록 설정.
    - 환경 변수 관리
        - 중요한 설정 정보는 `application-secret.yml`로 분리하고, GitHub Secrets를 활용하여 `APPLICATION_SECRET`, `GOOGLE_CLIENT_ID` 등 민감한 환경 변수를 안전하게 관리.
        - 배포 시 React 환경 변수를 설정하여 적절한 API URL과 클라이언트 ID를 사용하도록 구성.
    - 배포
        - Amazon Route 53을 통해 도메인 연결을 설정하고, Amazon Certificate Manager를 사용하여 HTTPS 지원.

--- src/main/java/com/spring/myapp/common/controller/HealthCheckController.java
+++ src/main/java/com/spring/myapp/common/controller/HealthCheckController.java
@@ -8,18 +8,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+/**
+ * 서버의 상태를 확인하기 위한 HealthCheckController 클래스입니다.
+ * 이 클래스는 서버의 현재 환경 및 설정된 정보를 반환하는 API를 제공합니다.
+ */
 @RestController
+@Tag(name = "Health Check Controller", description = "서버 상태 확인을 위한 API")
 public class HealthCheckController {
 
 	@Value("${server.env}")
 	private String env;
+
 	@Value("${server.port}")
 	private String serverPort;
+
 	@Value("${server.serverAddress}")
 	private String serverAddress;
+
 	@Value("${serverName}")
 	private String serverName;
 
+	/**
+	 * 서버의 상태를 확인하는 API입니다.
+	 *
+	 * @return 서버의 상태 정보
+	 */
+	@Operation(summary = "서버 상태 확인", description = "서버의 현재 상태 및 환경 정보를 반환합니다.")
 	@GetMapping("/hc")
 	public ResponseEntity<?> healthCheck() {
 		Map<String, String> responseData = new TreeMap<>();
@@ -30,6 +47,12 @@ public class HealthCheckController {
 		return ResponseEntity.ok(responseData);
 	}
 
+	/**
+	 * 서버의 현재 환경(env)을 반환하는 API입니다.
+	 *
+	 * @return 현재 환경(env) 정보
+	 */
+	@Operation(summary = "현재 환경 확인", description = "서버의 현재 환경(env) 정보를 반환합니다.")
 	@GetMapping("/env")
 	public ResponseEntity<?> getEnv() {
 		return ResponseEntity.ok(env);
